3983045f84d57147d87e4d20c17a53d75cfbd9e5
Brett <brettgraham@gmail.com>
2022-12-09 12:58:26 -0500

    unfixture asdf_version

--- tests/test_subprocess.py
+++ tests/test_subprocess.py
@@ -2,12 +2,12 @@ import pytest
 import virtualenv
 
 
-PATCH_VERSIONS = ["1", "2"]
-
-
-@pytest.fixture(scope="module", params=PATCH_VERSIONS)
-def asdf_version(request):
-    return request.param
+# PATCH_VERSIONS = ["1", "2"]
+# 
+# 
+# @pytest.fixture(scope="module", params=PATCH_VERSIONS)
+# def asdf_version(request):
+#     return request.param
 
 
 # @pytest.fixture(scope="module")
@@ -18,6 +18,7 @@ def asdf_version(request):
 
 
 #def test_run_subprocess(asdf_version, env_path):
+@pytest.mark.parametrize("asdf_version", ["1", "2"])
 def test_run_subprocess(asdf_version):
     path = f".tmp_{asdf_version}"
     virtualenv.cli_run([str(path)])
