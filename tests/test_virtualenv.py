f1138cb70dea859145c3b3ec5012decb9fbc5112
Brett <brettgraham@gmail.com>
2022-12-09 13:10:14 -0500

    run virtualenv in subprocess

--- tests/test_virtualenv.py
+++ tests/test_virtualenv.py
@@ -1,9 +1,11 @@
+import subprocess
+
 import pytest
-import virtualenv
+#import virtualenv
 
 
 @pytest.mark.parametrize("version", ["1", "2"])
 def test_run_virtualenv(version):
     path = f".tmp_{version}"
-    with virtualenv.cli_run([str(path)]):
-        pass
+    #virtualenv.cli_run([str(path)])
+    assert subprocess.run(["virtualenv", str(path)]).returncode == 0
