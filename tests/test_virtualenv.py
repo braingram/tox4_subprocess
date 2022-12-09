c5f8a45e40aa154cd95ee14e2325eff7bdde0e3c
Brett <brettgraham@gmail.com>
2022-12-09 13:31:25 -0500

    run virtualenv, this should fail first test

--- tests/test_virtualenv.py
+++ tests/test_virtualenv.py
@@ -7,5 +7,5 @@ import virtualenv
 @pytest.mark.parametrize("version", ["1", "2"])
 def test_run_virtualenv(version):
     path = f".tmp_{version}"
-    #virtualenv.cli_run([str(path)])
-    assert subprocess.run(["virtualenv", str(path)]).returncode == 0
+    virtualenv.cli_run([str(path)])
+    #assert subprocess.run(["virtualenv", str(path)]).returncode == 0
