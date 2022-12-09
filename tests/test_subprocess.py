7cca335b236627fe850feee68f4a54027bde1f05
Brett <brettgraham@gmail.com>
2022-12-09 12:10:32 -0500

    remove virtualenv

--- tests/test_subprocess.py
+++ tests/test_subprocess.py
@@ -15,31 +15,32 @@ def asdf_version(request):
     return request.param
 
 
-def env_run(env_path, command, *args, **kwargs):
-    """
-    Run a command on the context of the virtual environment at
-    the specified path.
-    """
-    print(f"env_run({env_path}, {command}, {args}, {kwargs})")
-    kwargs["bufsize"] = 0
-    #return subprocess.run([env_path / "bin" / command] + list(args), **kwargs).returncode == 0
+# def env_run(env_path, command, *args, **kwargs):
+#     """
+#     Run a command on the context of the virtual environment at
+#     the specified path.
+#     """
+#     print(f"env_run({env_path}, {command}, {args}, {kwargs})")
+#     kwargs["bufsize"] = 0
+#     #return subprocess.run([env_path / "bin" / command] + list(args), **kwargs).returncode == 0
+# 
+# 
+# @pytest.fixture(scope="module")
+# def env_path(asdf_version, tmp_path_factory):
+#     """
+#     Path to the virtualenv where the (old) asdf library is installed.
+#     """
+#     path = tmp_path_factory.mktemp(f"asdf-{asdf_version}-env", numbered=False)
+#     print(f"env_path {asdf_version} {path}")
+# 
+#     virtualenv.cli_run([str(path)])
+# 
+#     env_run(path, "python3", "--version")
+#     # env_run(path, "pip", "install", "requests")
+#     # assert env_run(path, "pip", "install", f"asdf=={asdf_version}"), f"Failed to install asdf version {asdf_version}"
+#     return path
 
 
-@pytest.fixture(scope="module")
-def env_path(asdf_version, tmp_path_factory):
-    """
-    Path to the virtualenv where the (old) asdf library is installed.
-    """
-    path = tmp_path_factory.mktemp(f"asdf-{asdf_version}-env", numbered=False)
-    print(f"env_path {asdf_version} {path}")
-
-    virtualenv.cli_run([str(path)])
-
-    env_run(path, "python3", "--version")
-    # env_run(path, "pip", "install", "requests")
-    # assert env_run(path, "pip", "install", f"asdf=={asdf_version}"), f"Failed to install asdf version {asdf_version}"
-    return path
-
-
-def test_run_subprocess(asdf_version, env_path, tmpdir):
+#def test_run_subprocess(asdf_version, env_path, tmpdir):
+def test_run_subprocess(asdf_version, tmpdir):
     print("do nothing")
