81e94fae2a53b82589c5ea717116505b58e6ba68
Brett <brettgraham@gmail.com>
2022-12-09 10:20:38 -0500

    adding virtualenv

--- tests/test_subprocess.py
+++ tests/test_subprocess.py
@@ -1,8 +1,14 @@
 import subprocess
 
 import pytest
+import pytest_remotedata
+import virtualenv
 
 
 @pytest.mark.parametrize("msg", ["1", "2"])
-def test_run_subprocess(msg):
-    assert subprocess.run(["python3", "-c", f"'print({msg})'"]).returncode == 0
+def test_run_subprocess(msg, tmp_path_factory):
+    path = tmp_path_factory.mktemp(f"foo_{msg}", numbered=False)
+
+    virtualenv.cli_run([str(path)])
+
+    assert subprocess.run(["echo", str(path)]).returncode == 0
