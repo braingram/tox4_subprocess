4e02735c46948600b5d9ed657e996bd009acad10
Brett <brettgraham@gmail.com>
2022-12-09 13:35:18 -0500

    add another 'version'

--- tests/test_virtualenv.py
+++ tests/test_virtualenv.py
@@ -4,7 +4,7 @@ import pytest
 import virtualenv
 
 
-@pytest.mark.parametrize("version", ["1", "2"])
+@pytest.mark.parametrize("version", ["0", "1", "2"])
 def test_run_virtualenv(version):
     path = f".tmp_{version}"
     virtualenv.cli_run([str(path)])
