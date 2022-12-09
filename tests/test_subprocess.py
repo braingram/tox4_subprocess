d4ff292766ebb210a67bc7e300041875038cf42e
Brett <brettgraham@gmail.com>
2022-12-09 12:25:47 -0500

    move cwd to conftest

--- tests/test_subprocess.py
+++ tests/test_subprocess.py
@@ -16,23 +16,23 @@ def asdf_version(request):
     return request.param
 
 
-@pytest.fixture(scope="session", autouse=True)
-def temp_cwd(tmpdir_factory):
-    """
-    This fixture creates a temporary current working directory
-    for the test session, so that docstring tests that write files
-    don't clutter up the real cwd.
-    """
-    original_cwd = os.getcwd()
-    try:
-        path = tmpdir_factory.mktemp("cwd")
-        print(f"temp_cwd {path} {original_cwd}")
-        os.chdir(path)
-        # os.chdir(tmpdir_factory.mktemp("cwd"))
-        yield
-    finally:
-        print(f"-- temp_cwd back to {original_cwd}")
-        os.chdir(original_cwd)
+# @pytest.fixture(scope="session", autouse=True)
+# def temp_cwd(tmpdir_factory):
+#     """
+#     This fixture creates a temporary current working directory
+#     for the test session, so that docstring tests that write files
+#     don't clutter up the real cwd.
+#     """
+#     original_cwd = os.getcwd()
+#     try:
+#         path = tmpdir_factory.mktemp("cwd")
+#         print(f"temp_cwd {path} {original_cwd}")
+#         os.chdir(path)
+#         # os.chdir(tmpdir_factory.mktemp("cwd"))
+#         yield
+#     finally:
+#         print(f"-- temp_cwd back to {original_cwd}")
+#         os.chdir(original_cwd)
 
 
 # def env_run(env_path, command, *args, **kwargs):
