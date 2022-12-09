e7814e11107d8e9602e9e837d59d827994c106d2
Brett <brettgraham@gmail.com>
2022-12-09 13:02:16 -0500

    typo

--- tests/test_virtualenv.py
+++ tests/test_virtualenv.py
@@ -4,5 +4,5 @@ import virtualenv
 
 @pytest.mark.parametrize("version", ["1", "2"])
 def test_run_virtualenv(version):
-    path = f".tmp_{asdf_version}"
+    path = f".tmp_{version}"
     virtualenv.cli_run([str(path)])
