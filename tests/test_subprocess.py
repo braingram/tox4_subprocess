fbb2bc2d850f82b88eddf70a4e910409a29440f3
Brett <brettgraham@gmail.com>
2022-12-09 12:49:32 -0500

    simplify

--- tests/test_subprocess.py
+++ tests/test_subprocess.py
@@ -1,6 +1,3 @@
-import os
-import subprocess
-
 import pytest
 import virtualenv
 
@@ -10,35 +7,13 @@ PATCH_VERSIONS = ["1", "2"]
 
 @pytest.fixture(scope="module", params=PATCH_VERSIONS)
 def asdf_version(request):
-    """
-    The (old) version of the asdf library under test.
-    """
     return request.param
-
-
-# def env_run(env_path, command, *args, **kwargs):
-#     """
-#     Run a command on the context of the virtual environment at
-#     the specified path.
-#     """
-#     print(f"env_run({env_path}, {command}, {args}, {kwargs})")
-#     kwargs["bufsize"] = 0
-#     #return subprocess.run([env_path / "bin" / command] + list(args), **kwargs).returncode == 0
 
 
 @pytest.fixture(scope="module")
 def env_path(asdf_version, tmp_path_factory):
-    """
-    Path to the virtualenv where the (old) asdf library is installed.
-    """
     path = tmp_path_factory.mktemp(f"asdf-{asdf_version}-env", numbered=False)
-    print(f"env_path {asdf_version} {path}")
-
     virtualenv.cli_run([str(path)])
-
-    #env_run(path, "python3", "--version")
-    # env_run(path, "pip", "install", "requests")
-    # assert env_run(path, "pip", "install", f"asdf=={asdf_version}"), f"Failed to install asdf version {asdf_version}"
     return path
 
 
